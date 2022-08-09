5fbe9cce12894045e0d61a2ebf1513a173fb0f87
Mahmudshahin <Shahinmahmud184@gmail.com>
2022-08-09 16:51:04 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqoaiDYqq5n6qUZP8qutNH_SdCE30qmKQ",
+  authDomain: "laptop-stock-management.firebaseapp.com",
+  projectId: "laptop-stock-management",
+  storageBucket: "laptop-stock-management.appspot.com",
+  messagingSenderId: "796115399443",
+  appId: "1:796115399443:web:e4f57a14c43f415dc660e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
